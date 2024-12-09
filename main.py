7b8994d6ca1cbe5a399a10dfba348b1b1d9b4e25
KRISHNA KUMAR VERMA <56273633+verma29897@users.noreply.github.com>
2024-12-09 14:38:08 +0530

    Update main.py

---
+++ main.py
@@ -0,0 +1,311 @@
+from utils.scrapper import get_barchart_downloads, create_bc_session
+
+import os
+import pandas as pd
+from os import listdir
+import matplotlib.pyplot as plt
+
+CONTRACT_MAP = {
+    "AEX": {"code": "AE", "cycle": "FGHJKMNQUVXZ", "exchange": "EuronextInd"},
+    "ALUMINIUM": {"code": "AL", "cycle": "FGHJKMNQUVXZ", "exchange": "COMEX"},
+    "ASX": {"code": "AP", "cycle": "FGHJKMNQUVXZ", "exchange": "SFE"},
+    "AUD": {"code": "A6", "cycle": "HMUZ", "exchange": "CME"},
+    "AUDJPY": {"code": "UK", "cycle": "HMUZ", "exchange": "CME"},
+    "BB3M": {"code": "BR", "cycle": "HMUZ", "exchange": "CME"},
+    "BBCOMM": {"code": "AH", "cycle": "HMUZ", "exchange": "CBOT"},
+    "BEL20": {"code": "BE", "cycle": "FGHJKMNQUVXZ", "exchange": "EuronextInd"},
+    "BITCOIN": {"code": "BA", "cycle": "FGHJKMNQUVXZ", "exchange": "CME"},
+    "BOBL": {"code": "HR", "cycle": "HMUZ", "exchange": "EUREX"},
+    "BONO": {"code": "IZ", "cycle": "HMUZ", "exchange": "EUREX"},
+    "BOVESPA": {"code": "IF", "cycle": "GJMQVZ", "exchange": "CME"},
+    "BRE": {"code": "L6", "cycle": "FGHJKMNQUVXZ", "exchange": "CME"},
+    "BRENT": {"code": "QA", "cycle": "FGHJKMNQUVXZ", "exchange": "NYMEX"},
+    "BRENT_W": {"code": "CB", "cycle": "FGHJKMNQUVXZ", "exchange": "ICE/EU/Com"},
+    "BRENT-LAST": {"code": "TBZ", "cycle": "FGHJKMNQUVXZ", "exchange": "NYMEX"},
+    "BTP": {"code": "II", "cycle": "HMUZ", "exchange": "EUREX"},
+    "BTP3": {"code": "IA", "cycle": "HMUZ", "exchange": "EUREX"},
+    "BUND": {"code": "GG", "cycle": "HMUZ", "exchange": "EUREX"},
+    "BUTTER": {"code": "BD", "cycle": "FGHJKMNQUVXZ", "exchange": "CME"},
+    "BUXL": {"code": "GX", "cycle": "HMUZ", "exchange": "EUREX"},
+    "CAC": {"code": "MX", "cycle": "FGHJKMNQUVXZ", "exchange": "EuronextInd"},
+    "CAD": {"code": "D6", "cycle": "HMUZ", "exchange": "CME"},
+    "CADJPY": {"code": "UF", "cycle": "HMUZ", "exchange": "CME"},
+    "CAD10": {"code": "CG", "cycle": "HMUZ", "exchange": "TMX"},
+    "CANOLA": {"code": "RS", "cycle": "FHKNX", "exchange": "ICE/CA"},
+    "CH10": {"code": "CF", "cycle": "HMUZ", "exchange": "EUREX"},
+    "CHEESE": {"code": "BJ", "cycle": "FGHJKMNQUVXZ", "exchange": "CME"},
+    "CHF": {"code": "S6", "cycle": "HMUZ", "exchange": "CME"},
+    "CHFJPY": {"code": "UP", "cycle": "HMUZ", "exchange": "CME"},
+    "CLP": {"code": "N5", "cycle": "FGHJKMNQUVXZ", "exchange": "CME"},
+    "CNH": {"code": "ZX", "cycle": "FGHJKMNQUVXZ", "exchange": "ICE/SG"},
+    "CNH-onshore": {"code": "ZP", "cycle": "FGHJKMNQUVXZ", "exchange": "ICE/SG"},
+    "COCOA_LDN": {"code": "CA", "cycle": "HKNUZ", "exchange": "ICE/EU/Com"},
+    "COCOA_NY": {"code": "CC", "cycle": "HKNUZ", "exchange": "ICE/US"},
+    "COFFEE": {"code": "KC", "cycle": "HKNUZ", "exchange": "ICE/US"},
+    "COPPER": {"code": "HG", "cycle": "FHJMNUVZ", "exchange": "COMEX"},
+    "COPPER-micro": {"code": "QL", "cycle": "HKNUZ", "exchange": "COMEX"},
+    "CORN": {"code": "ZC", "cycle": "HKNUZ", "exchange": "CBOT"},
+    "COTTON": {"code": "KG", "cycle": "HKNVZ", "exchange": "NYMEX"},
+    "COTTON2": {"code": "CT", "cycle": "HKNVZ", "exchange": "ICE/US"},
+    "CRUDE_ICE": {"code": "WI", "cycle": "FGHJKMNQUVXZ", "exchange": "ICE/EU/Com"},
+    "CRUDE_W": {"code": "CL", "cycle": "FGHJKMNQUVXZ", "exchange": "NYMEX"},
+    "CZK": {"code": "WC", "cycle": "HMUZ", "exchange": "CME"},
+    "DAX": {"code": "DY", "cycle": "HMUZ", "exchange": "EUREX"},
+    "DIVDAX-DIVI": {"code": "AI", "cycle": "HMUZ", "exchange": "EUREX"},
+    "DJSTX-SMALL": {"code": "PT", "cycle": "HMUZ", "exchange": "EUREX"},
+    "DOW": {"code": "YM", "cycle": "HMUZ", "exchange": "CBOT"},
+    "DX": {"code": "DX", "cycle": "HMUZ", "exchange": "ICE/US"},
+    "ETHANOL": {"code": "ZK", "cycle": "FGHJKMNQUVXZ", "exchange": "CBOT"},
+    "ETHEREUM": {"code": "ER", "cycle": "FGHJKMNQUVXZ", "exchange": "CME"},
+    "EUA": {
+        "code": "CK",
+        "cycle": "FGHJKMNQUVXZ",
+        "days_count": 800,
+        "exchange": "ENDEX",
+    },
+    "EUR": {"code": "E6", "cycle": "FGHJKMNQUVXZ", "exchange": "CME"},
+    "EURAUD": {"code": "UC", "cycle": "HMUZ", "exchange": "CME"},
+    "EURCAD": {"code": "EP", "cycle": "HMUZ", "exchange": "ICE/US"},
+    "EURCHF": {"code": "RF", "cycle": "HMUZ", "exchange": "CME"},
+    "EURGBP": {"code": "RP", "cycle": "HMUZ", "exchange": "CME"},
+    "EURIBOR": {"code": "TV", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EURIBOR-ICE": {
+        "code": "IM",
+        "cycle": "HMUZ",
+        "days_count": 800,
+        "exchange": "ICE",
+    },
+    "EUROSTX": {"code": "FX", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EUROSTX-LARGE": {"code": "PF", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EUROSTX-SMALL": {"code": "PI", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EUROSTX200-LARGE": {"code": "PO", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EURO600": {"code": "FY", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EU-AUTO": {"code": "S5", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EU-BASIC": {"code": "UJ", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EU-BANKS": {"code": "FA", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EU-CHEM": {"code": "C1", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EU-CONSTRUCTION": {"code": "C1", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EU-DIV50": {"code": "DO", "cycle": "MZ", "exchange": "EUREX"},
+    "EU-DIV30": {"code": "AQ", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EU-DJ-OIL": {"code": "D9", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EU-DJ-TECH": {"code": "UO", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EU-DJ-TELECOM": {"code": "J5", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EU-DJ-UTIL": {"code": "IE", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EU-HEALTH": {"code": "YS", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EU-INSURE": {"code": "DML", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EU-MEDIA": {"code": "UQ", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EU-MID": {"code": "PG", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EU-REALESTATE": {"code": "M4", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EU-OIL": {"code": "UH", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EU-TECH": {"code": "UO", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EU-TRAVEL": {"code": "J3", "cycle": "HMUZ", "exchange": "EUREX"},
+    "EU-UTILS": {"code": "J4", "cycle": "HMUZ", "exchange": "EUREX"},
+    "FANG": {"code": "FG", "cycle": "HMUZ", "exchange": "ICE/US"},
+    "FED": {"code": "ZQ", "cycle": "FGHJKMNQUVXZ", "exchange": "CBOT"},
+    "FEEDCOW": {"code": "GF", "cycle": "FHJKQUVX", "exchange": "CME"},
+    "FTSE100": {"code": "X", "cycle": "HMUZ", "exchange": "ICE"},
+    "FTSE250": {"code": "Y", "cycle": "HMUZ", "exchange": "ICE"},
+    "FTSECHINAA": {"code": "HN", "cycle": "FGHJKMNQUVXZ", "exchange": "SGX"},
+    "FTSECHINAH": {"code": "HA", "cycle": "FGHJKMNQUVXZ", "exchange": "HKFE"},
+    "FTSETAIWAN": {"code": "T1", "cycle": "FGHJKMNQUVXZ", "exchange": "SGX"},
+    "GAS-LAST": {"code": "HH", "cycle": "FGHJKMNQUVXZ", "exchange": "NYMEX"},
+    "GAS_NL": {"code": "TG", "cycle": "FGHJKMNQUVXZ", "exchange": "ENDEX"},
+    "GAS_UK": {"code": "NF", "cycle": "FGHJKMNQUVXZ", "exchange": "ICE/EU/Com"},
+    "GAS_US": {"code": "NG", "cycle": "FGHJKMNQUVXZ", "exchange": "NYMEX"},
+    "GAS_US_mini": {"code": "QG", "cycle": "FGHJKMNQUVXZ", "exchange": "NYMEX"},
+    "GASOIL": {"code": "LF", "cycle": "FGHJKMNQUVXZ", "exchange": "ICE/EU/Com"},
+    "GASOILINE": {"code": "RB", "cycle": "FGHJKMNQUVXZ", "exchange": "NYMEX"},
+    "GAS-PEN": {"code": "HP", "cycle": "FGHJKMNQUVXZ", "exchange": "NYMEX"},
+    "GBP": {"code": "B6", "cycle": "HMUZ", "exchange": "CME"},
+    "GBPCHF": {"code": "SS", "cycle": "HMUZ", "exchange": "ICE/US"},
+    "GBPEUR": {"code": "RP", "cycle": "HMUZ", "exchange": "CME"},
+    "GBPJPY": {"code": "UR", "cycle": "HMUZ", "exchange": "CME"},
+    "GICS": {"code": "GD", "cycle": "FGHJKMNQUVXZ", "exchange": "CME"},
+    "GILT": {"code": "G", "cycle": "HMUZ", "exchange": "ICE"},
+    "GOLD": {"code": "GC", "cycle": "GJMQVZ", "exchange": "COMEX"},
+    "GOLD_micro": {"code": "GR", "cycle": "GJMQVZ", "exchange": "COMEX"},
+    "HANG": {"code": "HS", "cycle": "FGHJKMNQUVXZ", "exchange": "HKFE"},
+    "HANGENT_mini": {"code": "IX", "cycle": "FGHJKMNQUVXZ", "exchange": "HKFE"},
+    "HANGTECH": {"code": "HI", "cycle": "FGHJKMNQUVXZ", "exchange": "HKFE"},
+    "HEATOIL": {"code": "HO", "cycle": "FGHJKMNQUVXZ", "exchange": "NYMEX"},
+    "HEATOIL-ICE": {"code": "LO", "cycle": "FGHJKMNQUVXZ", "exchange": "ICE/EU/Com"},
+    "HOUSE-US": {"code": "I6", "cycle": "FKQX", "exchange": "CME"},
+    "IBEX": {"code": "EF", "cycle": "HMUZ", "exchange": "MEFF"},
+    "IRON": {"code": "C0", "cycle": "FGHJKMNQUVXZ", "exchange": "SGX"},
+    "INR": {"code": "U-", "cycle": "FGHJKMNQUVXZ", "exchange": "SGX"},
+    "IRS": {"code": "WY", "cycle": "HMUZ", "exchange": "CME"},
+    "JGB": {"code": "JX", "cycle": "HMUZ", "exchange": "SGX"},
+    "JGB-SGX-mini": {"code": "JX", "cycle": "HMUZ", "exchange": "SGX"},
+    "JPY": {"code": "J6", "cycle": "HMUZ", "exchange": "CME"},
+    "JP-REALESTATE": {"code": "V.", "cycle": "HMUZ", "exchange": "JPX"},
+    "JSE40": {"code": "LX", "cycle": "HMUZ", "exchange": "SAFEX"},
+    "KRWUSD": {"code": "K9", "cycle": "FGHJKMNQUVXZ", "exchange": "SGX"},
+    "LEANHOG": {"code": "HE", "cycle": "GJKMNQVZ", "exchange": "CME"},
+    "LIVECOW": {"code": "LE", "cycle": "GJMQVZ", "exchange": "CME"},
+    "LUMBER-new": {"code": "LB", "cycle": "FHKNUX", "exchange": "CME"},
+    # Replace MSCIASIA with EUREX symbol if available to match pysystemtrade
+    "MSCIASIA": {"code": "L5", "cycle": "HMUZ", "exchange": "ICE/US"},
+    "MSCISING": {"code": "SV", "cycle": "FGHJKMNQUVXZ", "exchange": "SGX"},
+    "MID-DAX": {"code": "DM", "cycle": "HMUZ", "exchange": "EUREX"},
+    "MILK": {"code": "DL", "cycle": "FGHJKMNQUVXZ", "exchange": "CME"},
+    "MILKDRY": {"code": "DF", "cycle": "FGHJKMNQUVXZ", "exchange": "CME"},
+    "MILKWET": {"code": "DK", "cycle": "FGHJKMNQUVXZ", "exchange": "CME"},
+    "MILLWHEAT": {"code": "ML", "cycle": "HKUZ", "exchange": "EuronextCom"},
+    "MSCIWORLD": {"code": "AC", "cycle": "HMUZ", "exchange": "EUREX"},
+    "MXP": {"code": "M6", "cycle": "HMUZ", "exchange": "CME"},
+    "MUMMY": {"code": "S.", "cycle": "HMUZ", "exchange": "JPX"},
+    "NASDAQ": {"code": "NQ", "cycle": "HMUZ", "exchange": "CME"},
+    "NASDAQ_micro": {"code": "NM", "cycle": "HMUZ", "exchange": "CME"},
+    "NIFTY": {"code": "NH", "cycle": "FGHJKMNQUVXZ", "exchange": "SGX"},
+    "NIKKEI": {"code": "NY", "cycle": "HMUZ", "exchange": "CME"},
+    "NIKKEI400": {"code": "OC", "cycle": "HMUZ", "exchange": "JPX"},
+    "NOK": {"code": "NR", "cycle": "HMUZ", "exchange": "CME"},
+    "NZD": {"code": "N6", "cycle": "HMUZ", "exchange": "CME"},
+    "OAT": {"code": "FN", "cycle": "HMUZ", "exchange": "EUREX"},
+    "OATIES": {"code": "ZO", "cycle": "HKNUZ", "exchange": "CBOT"},
+    "OJ": {"code": "OJ", "cycle": "FHKNUX", "exchange": "ICE/US"},
+    "OMX": {"code": "FP", "cycle": "HMUZ", "exchange": "EUREX"},
+    "OMXS30": {"code": "OX", "cycle": "FGHJKMNQUVXZ", "exchange": "OMX"},
+    "PALLAD": {"code": "PA", "cycle": "HMUZ", "exchange": "NYMEX"},
+    "PLAT": {"code": "PL", "cycle": "FJNV", "exchange": "NYMEX"},
+    "PLN": {"code": "WP", "cycle": "HMUZ", "exchange": "CME"},
+    "R1000": {"code": "TX", "cycle": "HMUZ", "exchange": "CME"},
+    "RAPESEED": {"code": "XR", "cycle": "GKQX", "exchange": "EuronextCom"},
+    "REDWHEAT": {"code": "KE", "cycle": "HKNUZ", "exchange": "KCBT"},
+    "RICE": {"code": "ZR", "cycle": "FHKNUX", "exchange": "CBOT"},
+    "ROBUSTA": {"code": "RM", "cycle": "FHKNUX", "exchange": "ICE/EU/Com"},
+    "RUBBER": {"code": "W2", "cycle": "FGHJKMNQUVXZ", "exchange": "SGX"},
+    "RUR": {"code": "R6", "cycle": "HMUZ", "exchange": "CME"},
+    "RUSSELL": {"code": "QR", "cycle": "HMUZ", "exchange": "CME"},
+    "SEK": {"code": "SK", "cycle": "HMUZ", "exchange": "CME"},
+    "SGD_mini": {"code": "R$", "cycle": "FGHJKMNQUVXZ", "exchange": "SGX"},
+    "SGX": {"code": "VS", "cycle": "FGHJKMNQUVXZ", "exchange": "SGX"},
+    "SHATZ": {"code": "HF", "cycle": "HMUZ", "exchange": "EUREX"},
+    "SILVER": {"code": "SI", "cycle": "HKNUZ", "exchange": "COMEX"},
+    "SMI": {"code": "SZ", "cycle": "HMUZ", "exchange": "EUREX"},
+    "SMI-MID": {"code": "SH", "cycle": "HMUZ", "exchange": "EUREX"},
+    "SOFR": {"code": "SQ", "cycle": "HMUZ", "days_count": 1100, "exchange": "CME"},
+    "SONIA3": {"code": "MZ", "cycle": "HMUZ", "days_count": 1100, "exchange": "CME"},
+    "SOYBEAN": {"code": "ZS", "cycle": "FHKNQUX", "exchange": "CBOT"},
+    "SOYBEAN_mini": {"code": "XK", "cycle": "FHKNQUX", "exchange": "CBOT"},
+    "SOYMEAL": {"code": "ZM", "cycle": "FHKNQUVZ", "exchange": "CBOT"},
+    "SOYOIL": {"code": "ZL", "cycle": "FHKNQUVZ", "exchange": "CBOT"},
+    "SP400": {"code": "EW", "cycle": "HMUZ", "exchange": "CME"},
+    "SP500": {"code": "ES", "cycle": "HMUZ", "exchange": "CME"},
+    "SP500_micro": {"code": "ET", "cycle": "HMUZ", "exchange": "CME"},
+    "STEEL": {"code": "HV", "cycle": "FGHJKMNQUVXZ", "exchange": "COMEX"},
+    "SUGAR": {"code": "SW", "cycle": "HKQVZ", "exchange": "ICE/EU/Com"},
+    "SUGAR11": {"code": "SB", "cycle": "HKNV", "days_count": 200, "exchange": "ICE/US"},
+    "SWISSLEAD": {"code": "ST", "cycle": "HMUZ", "exchange": "EUREX"},
+    "TECDAX": {"code": "DZ", "cycle": "HMUZ", "exchange": "EUREX"},
+    "TOPIX": {"code": "TS", "cycle": "HMUZ", "exchange": "JPX"},
+    "US10": {"code": "ZN", "cycle": "HMUZ", "exchange": "CBOT"},
+    "US10U": {"code": "TN", "cycle": "HMUZ", "exchange": "CBOT"},
+    "US2": {"code": "ZT", "cycle": "HMUZ", "exchange": "CBOT"},
+    "US30": {"code": "UD", "cycle": "HMUZ", "exchange": "CBOT"},
+    "US30Y": {"code": "ZB", "cycle": "HMUZ", "exchange": "CBOT"},
+    "US5": {"code": "ZF", "cycle": "HMUZ", "exchange": "CBOT"},
+    "US-DISCRETE": {"code": "BI", "cycle": "HMUZ", "exchange": "CME"},
+    "US-ENERGY": {"code": "BM", "cycle": "HMUZ", "exchange": "CME"},
+    "US-FINANCE": {"code": "BN", "cycle": "HMUZ", "exchange": "CME"},
+    "US-HEALTH": {"code": "BS", "cycle": "HMUZ", "exchange": "CME"},
+    "US-INDUSTRY": {"code": "JG", "cycle": "HMUZ", "exchange": "CME"},
+    "US-MATERIAL": {"code": "JA", "cycle": "HMUZ", "exchange": "CME"},
+    "US-PROPERTY": {"code": "BK", "cycle": "HMUZ", "exchange": "CME"},
+    "US-REALESTATE": {"code": "DH", "cycle": "HMUZ", "exchange": "CBOT"},
+    "US-STAPLES": {"code": "BL", "cycle": "HMUZ", "exchange": "CME"},
+    "US-TECH": {"code": "JB", "cycle": "HMUZ", "exchange": "CME"},
+    "US-UTILS": {"code": "JE", "cycle": "HMUZ", "exchange": "CME"},
+    "US20": {"code": "ZZ", "cycle": "HMUZ", "exchange": "CBOT"},
+    "US3": {"code": "ZE", "cycle": "HMUZ", "exchange": "CBOT"},
+    "V2X": {"code": "DV", "cycle": "FGHJKMNQUVXZ", "exchange": "EUREX"},
+    "VIX": {"code": "VI", "cycle": "FGHJKMNQUVXZ", "exchange": "CFE"},
+    "VNKI": {"code": "Y.", "cycle": "FGHJKMNQUVXZ", "exchange": "JPX"},
+    "WHEAT": {"code": "ZW", "cycle": "HKNUZ", "exchange": "CBOT"},
+    "WHEAT_ICE": {"code": "LW", "cycle": "FHKNUX", "exchange": "ICE/EU/Com"},
+    "WHEY": {"code": "DG", "cycle": "FGHJKMNQUVXZ", "exchange": "CME"},
+    "YENEUR": {"code": "RY", "cycle": "HMUZ", "exchange": "CME"},
+    "ZAR": {"code": "T6", "cycle": "HMUZ", "exchange": "CME"},
+}
+
+CONTRACT_MAP['GOLD']
+CONTRACT_MAP['SILVER']
+# Define constants
+CONTRACT = 'METALS'
+start = 2018
+end = 2025
+path = f'utils/{CONTRACT}'
+
+# Ensure the directory exists
+os.makedirs(path, exist_ok=True)
+
+print(CONTRACT_MAP['GOLD'])
+print(CONTRACT_MAP['SILVER'])
+# Define contract map
+CONTRACTS = {
+    'GOLD': CONTRACT_MAP['GOLD'],
+    'SILVER': CONTRACT_MAP['SILVER'],
+}
+
+# Create a session with the Barchart credentials
+session = create_bc_session(
+    barchart_username="",
+    barchart_password=""
+)
+
+# Download data from Barchart
+get_barchart_downloads(
+    session=session,
+    contract_map=CONTRACTS,
+    save_dir=path,
+    start_year=start,
+    end_year=end
+)
+
+# Define month-to-contract code mapping
+CTR_MAP = {8: 'Q', 9: 'U', 10: 'V', 11: 'X', 12: 'Z'}
+
+# Initialize a list to hold dataframes
+data = []
+
+# Process each file in the directory
+for p in listdir(path):
+    print(f"Processing file: {p}")
+    year = int(p.split('_')[2][:4])
+    month = int(p.split('_')[2][4:6])
+
+    # Read the CSV file
+    df = pd.read_csv(f"{path}/{p}", index_col=0, parse_dates=True)
+    #df['Contract'] = f"{CONTRACT_MAP[CONTRACT]['code']}{year}{CTR_MAP[month]}"
+    data.append(df)
+
+# Concatenate all data into a single DataFrame
+ba = pd.concat(data)
+
+# Add a "Mean" column
+ba['Mean'] = (ba['High'] + ba['Low']) / 2
+
+
+
+
+# Plot "High", "Low", and "Mean" columns
+plt.figure(figsize=(10, 6))
+plt.plot(ba.index, ba['High'], label='High', color='blue')
+plt.plot(ba.index, ba['Low'], label='Low', color='green')
+plt.plot(ba.index, ba['Mean'], label='Mean', color='red')
+plt.title('High, Low, and Mean of Futures Data')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.legend()
+plt.grid()
+plt.tight_layout()
+
+# Save the plot as an image
+plt.savefig(f'{path}/futures_data_plot.png')
+plt.close()
+
+# Save the processed data to an Excel file
+excel_path = f'{path}/Futures_Market_Data.xlsx'
+with pd.ExcelWriter(excel_path, engine='openpyxl') as writer:
+    ba.to_excel(writer, sheet_name='Raw Data')
+    print(f"Data saved to Excel at {excel_path}")
